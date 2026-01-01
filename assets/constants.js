ef7ed94c55c37c4482ef30f7551ca4321fb9719a
Oladolapo Bello <dolapobello1@yahoo.com>
2026-01-01 14:56:15 -0500

    Update constants.js

--- assets/constants.js
+++ assets/constants.js
@@ -1,10 +1,6 @@
-// assets/constants.js
-
-// Consolidated Relief Allowance for Individuals
 export const CRA = 800000;
 export const MAX_RENT_RELIEF = 500000;
 
-// Personal Income Tax (PIT) Bands for Nigeria 2026
 export const PIT_BANDS = [
   [2200000, 0.15],
   [9000000, 0.18],
@@ -13,6 +9,6 @@ export const PIT_BANDS = [
   [Infinity, 0.25]
 ];
 
-// SME / Corporate Tax Rates
-export const SME_TAX_RATE = 0.30; // 30%
-export const CORPORATE_TAX_RATE = 0.30; // 30%
+// Corporate & SME rates
+export const SME_RATE = 0.20;
+export const CORPORATE_RATE = 0.30;
